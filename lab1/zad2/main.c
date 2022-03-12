34bfaf587f089e8645c7ea57eca043698bb4eaec
MaOlszewska <marto12@wp.pl>
2022-03-12 21:53:30 +0100

    3a done

--- lab1/zad2/main.c
+++ lab1/zad2/main.c
@@ -1,12 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include  "../zad1/lib_counting.h"
-
 #include <sys/times.h>
 #include <unistd.h>
 
-// zadania do wykonania: 1 - Zliczenie dla plików, 0 - usunięcie bloku o podanym indeksie
+#ifndef DYNAMIC
+    #include  "../zad1/lib_counting.h"
+#endif
+
+#ifdef DYNAMIC
+
+#include <dlfcn.h>
+typedef struct Block
+{
+    int rows_number;
+    char **rows;
+} Block;
+
+typedef struct ArrayOfPointers
+{
+    int size;
+    int las_idx;
+    struct Block** blocks;
+} ArrayOfPointers;
+
+struct ArrayOfPointers* (*create_array)(int);
+struct Block* (*create_counting_blocks)(char*);
+void (*remove_block)(struct ArrayOfPointers*, int);
+int(*count_files)(struct ArrayOfPointers*, char*);
+void (*remove_array)(struct ArrayOfPointers*);
+void (*remove_rows)(struct Block*);
+void (*show_block)(struct Block*);
+void (*show_array)(struct ArrayOfPointers*);
+#endif
 
 long double time_sec(clock_t time){
     return (long double)(time) / sysconf(_SC_CLK_TCK);
@@ -30,12 +56,22 @@ int command(char* task){
 
 int main(int argc, char* argv[]) {
 
+    #ifdef DYNAMIC
+    void *handle = dlopen("../zad1/lib_counting.so",RTLD_LAZY);
+    create_array = (struct ArrayOfPointers* (*) (int)) dlsym (handle, "create_array");
+    create_counting_blocks = (struct Block* (*)(char*)) dlsym (handle, "create_counting_blocks");
+    remove_block =(void (*) (struct ArrayOfPointers*, int)) dlsym (handle, "remove_block");
+    count_files =(int (*) (struct ArrayOfPointers*, char*)) dlsym (handle, "count_files");
+    remove_array =(void (*) (struct ArrayOfPointers*)) dlsym (handle, "remove_array");
+    remove_rows = (void (*) (struct Block*)) dlsym (handle, "remove_rows");
+    show_block = (void(*) (struct Block*)) dlsym (handle, "show_block");
+    show_array = (void (*) (struct ArrayOfPointers*)) dlsym (handle, "show_array");
+    #endif
+
     struct tms start_tms;
     struct tms end_tms;
     clock_t clock_start;
     clock_t clock_end;
-
-    //char* files[] = {"ok.txt sysop.txt jestfajnie.txt", "sysop.txt ok.txt","0"};
 
     struct ArrayOfPointers *array = NULL;
     array = create_array(argc - 1);
@@ -67,10 +103,10 @@ int main(int argc, char* argv[]) {
         clock_end = times(&end_tms);
         print_res(clock_start, clock_end, start_tms, end_tms);
     }
-    remove_array(array);
     clock_end = times(&end_tms);
     printf("TOTAL TIME: ");
     print_res(clock_start, clock_end, start_tms, end_tms);
+    remove_array(array);
 
     return 0;
 }
