c20615ce9b856b228efaafc290392406a1deacf1
Martyna Olszewska <marto12@wp.pl>
2022-04-30 14:27:35 +0200

    sysop

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/lab6/zad2/client.c.o"
+  "CMakeFiles/untitled.dir/lab6/zad2/client.c.o.d"
+  "CMakeFiles/untitled.dir/lab6/zad2/server.c.o"
+  "CMakeFiles/untitled.dir/lab6/zad2/server.c.o.d"
+  "untitled"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/martynka/CLionProjects/Sysopy
+# Install script for directory: /home/martyna/Pulpit/SYSOPY
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/martynka/CLionProjects/Sysopy/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/martyna/Pulpit/SYSOPY/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
